78c9be27067ddd49859440268c374bfda07143a2
Michael <kill.coder@gmail.com>
2019-03-08 16:28:44 +1030

    chore: Minor cleanup.

--- src/electricui/DebugInterface.tsx
+++ src/electricui/DebugInterface.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import ShowTransportWindowDevTools from './debug/ShowTransportWindowDevTools'
-
 const DebugInterface = () => (
   <div>Hey this is the debug screen and it hot reloads too!</div>
 )
