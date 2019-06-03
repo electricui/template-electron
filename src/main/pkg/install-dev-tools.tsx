07b828e3b1fa2c114d548c2cc842a592654e3a2e
Michael Orenstein <michael@whiteshadows.me>
2019-06-03 19:45:48 +0930

    feat: Setup protocol for arc to filter dev output

--- src/main/pkg/install-dev-tools.tsx
+++ src/main/pkg/install-dev-tools.tsx
@@ -6,12 +6,12 @@ import installExtension, {
 const installDevTools = () => {
   const forceDownload = !!process.env.UPGRADE_EXTENSIONS
 
-  installExtension(REACT_DEVELOPER_TOOLS, forceDownload)
-    .then(name => console.log(`Added Extension:  ${name}`))
-    .catch(err => console.log('An error occurred: ', err))
+  installExtension(REACT_DEVELOPER_TOOLS, forceDownload).catch(err =>
+    console.log('An error occurred: ', err),
+  )
 
-  installExtension(REDUX_DEVTOOLS, forceDownload)
-    .then(name => console.log(`Added Extension:  ${name}`))
-    .catch(err => console.log('An error occurred: ', err))
+  installExtension(REDUX_DEVTOOLS, forceDownload).catch(err =>
+    console.log('An error occurred: ', err),
+  )
 }
 export default installDevTools
