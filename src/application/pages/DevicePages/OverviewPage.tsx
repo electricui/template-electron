f7d679d2ad58ae0029528bf44e4da71bb593e3e4
Michael Orenstein <michael@whiteshadows.me>
2019-11-19 15:10:45 +1030

    chore: Switched overview page to use atomic layout

--- src/application/pages/DevicePages/OverviewPage.tsx
+++ src/application/pages/DevicePages/OverviewPage.tsx
@@ -1,60 +1,67 @@
+import { Card } from '@blueprintjs/core'
+import { Chart } from '@electricui/components-desktop-charts'
+import { Composition } from 'atomic-layout'
+import { IntervalRequester } from '@electricui/components-core'
+import { LightBulb } from '../../components/LightBulb'
 import React from 'react'
 import { RouteComponentProps } from '@reach/router'
-import { Card } from '@blueprintjs/core'
-import { Grid, Cell } from 'styled-css-grid'
-
-import { IntervalRequester } from '@electricui/components-core'
 import { Slider } from '@electricui/components-desktop-blueprint'
-import { Chart } from '@electricui/components-desktop-charts'
 
-import { LightBulb } from '../../components/LightBulb'
+const areas = `
+  ChartArea ChartArea
+  LightArea SliderArea
+`
 
 export const OverviewPage = (props: RouteComponentProps) => {
   return (
     <React.Fragment>
       <IntervalRequester interval={200} variables={['led_state']} />
 
-      <Grid columns={1}>
-        <Cell>
-          <Card>
-            <div style={{ textAlign: 'center', marginBottom: '1em' }}>
-              <b>LED State</b>
-            </div>
-            <Chart
-              timeseriesKey="led_state"
-              duration={28000}
-              delay={0}
-              hideLegend={true}
-              yMin={0}
-              yMax={1}
-              height={200}
-            />
-          </Card>
-        </Cell>
-        <Grid columns={2}>
-          <Cell>
-            <LightBulb
-              containerStyle={{ margin: '20px auto', width: '80%' }}
-              width="40vw"
-            />
-          </Cell>
-          <Cell>
-            <Card>
-              <div style={{ margin: 20 }}>
-                <Slider
-                  min={20}
-                  max={1020}
-                  stepSize={10}
-                  labelStepSize={100}
-                  sendOnlyOnRelease
-                >
-                  <Slider.Handle accessor="lit_time" />
-                </Slider>
-              </div>
-            </Card>
-          </Cell>
-        </Grid>
-      </Grid>
+      <Composition areas={areas}>
+        {({ ChartArea, LightArea, SliderArea }) => (
+          <React.Fragment>
+            <ChartArea>
+              <Card>
+                <div style={{ textAlign: 'center', marginBottom: '1em' }}>
+                  <b>LED State</b>
+                </div>
+                <Chart
+                  timeseriesKey="led_state"
+                  duration={28000}
+                  delay={0}
+                  hideLegend={true}
+                  yMin={0}
+                  yMax={1}
+                  height={200}
+                />
+              </Card>
+            </ChartArea>
+
+            <LightArea>
+              <LightBulb
+                containerStyle={{ margin: '20px auto', width: '80%' }}
+                width="40vw"
+              />
+            </LightArea>
+
+            <SliderArea>
+              <Card>
+                <div style={{ margin: 20 }}>
+                  <Slider
+                    min={20}
+                    max={1020}
+                    stepSize={10}
+                    labelStepSize={100}
+                    sendOnlyOnRelease
+                  >
+                    <Slider.Handle accessor="lit_time" />
+                  </Slider>
+                </div>
+              </Card>
+            </SliderArea>
+          </React.Fragment>
+        )}
+      </Composition>
     </React.Fragment>
   )
 }
