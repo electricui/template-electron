ec76a4d8235094f98c7a854be4825cc095be9f52
Michael Orenstein <michael@whiteshadows.me>
2020-12-03 12:52:14 +1030

    refactor: Expose less of the hot reload functionality in the template

--- src/transport-manager/config/index.tsx
+++ src/transport-manager/config/index.tsx
@@ -7,6 +7,7 @@ import {
   DeviceManager,
   Hint,
   MessageRouterLogRatioMetadata,
+  hotReloadDeviceManager,
 } from '@electricui/core'
 import { ProcessName, RequestName } from './metadata'
 import {
@@ -121,31 +122,4 @@ deviceManager.poll(cancellationToken).catch(err => {
   }
 })
 
-if (module.hot) {
-  /**
-   * This handler accepts disposal events in the hot reload system.
-   * It will tear down the device manager and record device and connection information,
-   * passing it to the next module so connections don't go away.
-   */
-  module.hot.dispose((data: any) => {
-    // record the devices we're currently connected to
-    const devices = deviceManager.getDevicesMap()
-
-    let deviceIDsWithUsageRequests = Array.from(devices.values())
-      .filter(device => device.getUsageRequests().includes('ui'))
-      .map(device => device.getDeviceID())
-
-    data['deviceIDsToConnectTo'] = deviceIDsWithUsageRequests
-  })
-
-  /**
-   * This handler accepts information from the old device manager and loads it in
-   */
-  if (module.hot.data && module.hot.data['deviceIDsToConnectTo']) {
-    let deviceIDsWithUsageRequests = module.hot.data['deviceIDsToConnectTo']
-
-    deviceManager.setDeviceIDsToAutoConnectForHotReloading(
-      deviceIDsWithUsageRequests,
-    )
-  }
-}
+hotReloadDeviceManager(deviceManager, module.hot)
